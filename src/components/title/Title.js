c6d5dbd2517e5546f15d13d56c01c567a759ede1
unknown <shaxrodgers@gmail.com>
2022-08-27 11:32:44 +0200

    first commit

---
+++ src/components/title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Title.css'
+
+const title = () => {
+  return (
+    <div className='title'>
+      <h1>How long till Kaylin comes home?</h1>
+      </div>
+  )
+}
+
+export default title
